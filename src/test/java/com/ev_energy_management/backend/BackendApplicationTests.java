cfa81981533662b5dc9c463dddef41e6a96f038a
suyeon <lsyeon030419@gmail.com>
2026-07-29 14:39:41 +0900

    Initial Spring Boot backend setup

---
+++ src/test/java/com/ev_energy_management/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ev_energy_management.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
